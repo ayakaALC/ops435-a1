0b7ebc2ce00dad275f8273757ca490c12af336ac
ayakaALC <47988963+ayakaALC@users.noreply.github.com>
2019-10-06 13:50:44 -0400

    Add files via upload

--- a1_ssha2.py
+++ a1_ssha2.py
@@ -1,11 +1,10 @@
 #!/usr/sbin/env python3
-
 """
 OPS435 Assignment 1 - Fall 2019
-Program: a1_[student_id].py (replace student_id with your Seneca User name)
-Author: "Student Name"
-The python code in this file (a1_[Student_id].py) is original work written by
-"Student Name". No code in this file is copied from any other source 
+Program: a1_ssha2.py
+Author: "Sang Soo Ha"
+The python code in this file (a1_ssha2.py) is original work written by
+"Sang Soo Ha". No code in this file is copied from any other source 
 except those provided by the course instructor, including any person, 
 textbook, or on-line resource. I have not shared this python script 
 with anyone or anything except for submission for grading.  
@@ -16,236 +15,199 @@ violators will be reported and appropriate action will be taken.
 import os,sys
 
 
+def usage():
+    """
+    This usage function is to check how many arguments were given, 
+    and to give a guidance to users that the the number of arguments
+    should be no more or less than 3 and 4. 
+    """
+
+    if len(sys.argv) !=3 or not 4:
+        print(f"{sys.argv[0]} [--step] YYYYMMDD +/-n")
+        sys.exit()
+
+
+def valid_date(date):
+    if len(date) != 10:
+        print("Error: wrong date entered")
+        sys.exit()
+    else:
+        year = int(date[:4])
+        month = int(date[5:7])
+        day = int(date[8:])
+        if month > 12: 
+            print("Error: wrong month entered")
+            sys.exit()
+        elif day >31:
+            print('Error: wrong day entered')
+            sys.exit()
+        return date
+
+
 def dbda(date, days):
+    '''
+    This function requires two parameters: date and entered days.
+    If the days are greater than 0, it will invoke the tomorrow function
+    If less than 0, it will invoke the yesterday function
+    '''
     #print('{},{}'.format(date,days))
     if days > 0:
-        after(date,days)
-        print('positive')
-        print(after(date,days))
-
+        for i in range(days):
+            date = tomorrow(date)
+            #print(date)
+        #print('positive')
+        #print(tomorrow(date,days))
+        print(date)
     elif days == 0:
         pass
-    #elif kwargs != 0:
-    #    for i in kwargs:
-    #        return(i * after(date,i))
 
     else:
-        before(date,days)
-        print('negative')
-        print(before(date,days))
+        #days = - days
+        for i in range(-days):
+            date = yesterday(date)
+        #print('negative')
+        #print(yesterday(date,days))
+        print(date)
+    return date
+    #print(date)
 
-def leap_year(year):
+def leap_year(lyear):
     
-    lyear = year % 4 
+    if (lyear % 4 == 0): 
+        if (lyear % 100 == 0):
+            if (lyear % 400 == 0):
+                leapyear = True
+            else: 
+                leapyear = False
+        else:
+            leapyear = True   
+    else: 
+         leapyear = False   
+    
+    return leapyear
 
-    if lyear == 0:
+
+def days_in_mon(year):
+
+    if leap_year(year):
         feb_max = 29
-
     else:
         feb_max = 28
 
-    lyear = year % 100 
-
-    if lyear == 0:
-        feb_max = 28
-
-        lyear - year % 400  
-
-    if lyear == 0:
-        feb_max = 29 
-
-    return feb_max
+    mon_max = {1:31, 2:feb_max, 3:31, 4:30, 5:31, \
+    6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+    return mon_max
 
 
-def after(date,days):   
+def tomorrow(date):   
     #print('testing after def')
-    if len(date) != 10:
-        #return '0000/00/00'
-        print('Error: wrong date entered')
-        sys.exit()
+    #if len(date) != 10:
+    #    #return '0000/00/00'
+    #    print('Error: wrong date entered')
+    #    sys.exit()
+    date = valid_date(date)
+    str_year, str_month, str_day = date.split('/')
+    year = int(str_year)
+    month = int(str_month)
+    day = int(str_day)
+    mon_max = days_in_mon(year)
+    
 
+    tmp_day = day + 1 # next day
+
+    if tmp_day > mon_max[month]:
+        to_day = tmp_day % mon_max[month] # if tmp_day > this month's max, reset to 1
+        tmp_month = month + 1
     else:
-        str_year, str_month, str_day = date.split('/')
-        year = int(str_year)
-        month = int(str_month)
-        day = int(str_day)
-        #print(year,month,day)
-        if month > 12:
-            print("Error: wrong month entered") 
-            sys.exit()
+      to_day = tmp_day
+      tmp_month = month + 0
 
-        elif day > 31:
-            print("Error: wrong day entered")
-            sys.exit()
+    if tmp_month > 12:
+       to_month = 1
+       year = year + 1
+    else:
+       to_month = tmp_month + 0
 
-        lyear = year % 4 
-
-        if lyear == 0:
-            feb_max = 29
-
-        else:
-            feb_max = 28
-
-        lyear = year % 100 
-
-        if lyear == 0:
-            feb_max = 28
-
-            lyear - year % 400  
-
-        if lyear == 0:
-            feb_max = 29 
-        
-        tmp_day = day + days
-
-        mon_max = {1:31, 2:feb_max, 3:31, 4:30, 5:31, \
-            6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
-        
-        #if days + mon_max[month] > mon_max[month]:
-        #    tmp_month = month + round(days / mon_max[month])
-        #    to_day = (day + mon_max[month]) % mon_max[month]
-        if tmp_day > mon_max[month]:
-            to_day = tmp_day % mon_max[month]
-            tmp_month = month + (-(-tmp_day // mon_max[month])) - 1
-            #to_day = tmp_day % mon_max[tmp_month]
-            
-        else:
-            to_day = tmp_day
-            tmp_month = month + 0 
-
-        #if day <= days
-
-        if tmp_month > 12:
-            to_month = tmp_month % 12
-            #to_day = (- to_month)+ round(tmp_day / mon_max[month])
-            year = year + tmp_month / 12
-            year = int(year)
-            new_year = leap_year(year)
-            print(new_year)
-            mon_max = {1:31, 2:new_year, 3:31, 4:30, 5:31, \
-            6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
-            to_day = round(tmp_day / mon_max[to_month])
-
-        #if days > year:
-        #    year = year + round((- days) / 365)
-        else:
-            to_month = tmp_month + 0
-        
-        next_date = str(year)+"/"+str(to_month).zfill(2)+"/"+str(to_day).zfill(2)
-
-        return next_date
+    next_date = str(year)+"/"+str(to_month).zfill(2)+"/"+str(to_day).zfill(2)
+     
+    return next_date
 
 
-def before(date,days):
-    print('Testing')
+def yesterday(date):
+    #print('Testing')
         #print('testing after def')
-    if len(date) != 10:
-        #return '0000/00/00'
-        print('Error: wrong date entered')
-        sys.exit()
-
-    else:
-        str_year, str_month, str_day = date.split('/')
-        year = int(str_year)
-        month = int(str_month)
-        day = int(str_day)
-        #print(year,month,day)
-        if month > 12:
-            print("Error: wrong month entered") 
-            sys.exit()
-
-        elif day > 31:
-            print("Error: wrong day entered")
-            sys.exit()
-
-        lyear = year % 4 
-
-        if lyear == 0:
-            feb_max = 29
-
-        else:
-            feb_max = 28
-
-        lyear = year % 100 
-
-        if lyear == 0:
-            feb_max = 28
-
-            lyear - year % 400  
-
-        if lyear == 0:
-            feb_max = 29 
+#    if len(date) != 10:
+#        #return '0000/00/00'
+#        print('Error: wrong date entered')
+#        sys.exit()
+    date = valid_date(date)
+    str_year, str_month, str_day = date.split('/')
+    year = int(str_year)
+    month = int(str_month)
+    day = int(str_day)
+    #print(day)
+    mon_max = days_in_mon(year)
         
-        tmp_day = day + days 
-        tmp_days = (- tmp_day)
-
-        mon_max = {1:31, 2:feb_max, 3:31, 4:30, 5:31, \
-        6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+    tmp_day = day - 1
+    #tmp_days = (- tmp_day)
         
         #tmp_day = month[max] + negative
 
-        if (- days) > day:
-            #tmp_month = month - 1
-            #if tmp_month < 1:
-            #    tmp_month = 12 - tmp_month
-            #else:    
-            tmp_month = month - (-(-tmp_days // mon_max[month]))
-            #to_day = day - round(tmp_days / mon_max[month])
-            to_day = mon_max[tmp_month] - tmp_days % mon_max[month]
-        
-        elif (- days) == 1:
-            tmp_month = month - 1
-            if month == 1:
-                tmp_month = 12
-                year = year - 1
-            to_day = mon_max[tmp_month] - tmp_days % mon_max[month]
-
-
+    if tmp_day == 0: 
+        month = month - 1
+        #tmp_month = month  
+        #tmp_month = to_month
+        if month == 0: 
+            #tmp_month = 12
+            year -= 1
+            month = 12
+            tmp_day = mon_max[month]
+            
         else:
-            to_day = tmp_day
-            tmp_month = month + 0
+            tmp_day = mon_max[month]
+        #tmp_month = month - 1
+        #if tmp_month < 1:
+        #    tmp_month = 12 - tmp_month
+        #else:    
+        #tmp_month = month - (-(-tmp_days // mon_max[month]))
+        #to_day = day - round(tmp_days / mon_max[month])
+        #to_day = mon_max[tmp_month] - tmp_days % mon_max[month]
+    else:
+        year -= 0
+        #tmp_day = tmp_day
+        month -= 0
 
-        #if (- days) > lyear: 
-        #    year = year - round((- days) / 365)
-        #    to_month = (- days) - month
+    """if tmp_month < 1:
+        to_month = (- tmp_month) % 12
+        to_day = mon_max[to_month] - tmp_days % mon_max[month]
+        year = year - tmp_month / 12
+        year = int(year)
 
-        #if day == tmp_days:
-        #    to_day = mon_max[tmp_month]
-        #    print('testing11')
-
-        if tmp_month < 1:
-            to_month = (- tmp_month) % 12
-            to_day = mon_max[to_month] - tmp_days % mon_max[month]
-            year = year - tmp_month / 12
-            year = int(year)
-
-        else:
-            to_month = tmp_month + 0
-            tmp_month = month + 0
+    else:
+        to_month = tmp_month + 0
+        tmp_month = month + 0"""
         
-        next_date = str(year)+"/"+str(to_month).zfill(2)+"/"+str(to_day).zfill(2)
+    next_date = str(year)+"/"+str(month).zfill(2)+"/"+str(tmp_day).zfill(2)
 
-        return next_date
+    return next_date
 
 if __name__ == "__main__":
-
-    try:
-        if len(sys.argv) > 1 and sys.argv[1] != '--step': 
-            date = sys.argv[1]
-            days = int(sys.argv[2])
-            dbda(date,days)
-
-        elif sys.argv[1] == '--step':
-            date = sys.argv[2]
-            days = int(sys.argv[3])
-            #steps = sys.argv[3]
-            #steps = str(days)
-
-            for i in range(1,days + 1):
-                #print(i)
-                dbda(date,i)
     
-    except IndexError:
-        print("Usage: ssha2.py [--step] YYYY/MM/DD +/-n")
+    usage()
+    if len(sys.argv) > 1 and sys.argv[1] != '--step': 
+        date = sys.argv[1]
+        days = int(sys.argv[2])
+        dbda(date,days)
+
+    elif sys.argv[1] == '--step':
+        date = sys.argv[2]
+        days = int(sys.argv[3])
+        #steps = sys.argv[3]
+        #steps = str(days)
+
+        for i in range(1,days + 1):
+            #print(i)
+            dbda(date,i)
+    
 
 
